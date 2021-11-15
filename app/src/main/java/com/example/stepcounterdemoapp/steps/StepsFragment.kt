862fa34673e28a0f47549bc610b951daad72b7be
Lucy Holub <lusicomgolub@gmail.com>
2021-11-15 14:37:46 +0200

    UI part designed

---
+++ app/src/main/java/com/example/stepcounterdemoapp/steps/StepsFragment.kt
@@ -0,0 +1,66 @@
+package com.example.stepcounterdemoapp.steps
+
+import android.os.Bundle
+import android.view.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.stepcounterdemoapp.R
+import com.example.stepcounterdemoapp.databinding.FragmentStepsBinding
+import android.widget.Toast
+import androidx.core.view.get
+import java.text.FieldPosition
+
+class StepsFragment : Fragment() {
+
+    private lateinit var viewModel: StepsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentStepsBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_steps,
+            container,
+            false
+        )
+
+        viewModel = ViewModelProvider(this).get(StepsViewModel::class.java)
+
+        viewModel.isStarted.observe(
+            viewLifecycleOwner,
+            Observer<Boolean> {
+                when (it) {
+                    true -> {
+                        binding.button.text = "Stop"
+                    }
+                    false -> {
+                        binding.button.text = "Start"
+                    }
+                }
+            })
+
+
+
+        binding.button.setOnClickListener {
+            viewModel.changeButtonState()
+        }
+
+        val fps = resources.getStringArray(R.array.fps_array)
+
+        val adapter = context?.let { ArrayAdapter(it, R.layout.dropdown_item, fps) }
+
+        binding.autoCompleteTextView.setAdapter(adapter)
+        binding.autoCompleteTextView.onItemClickListener =
+            AdapterView.OnItemClickListener{parent, view, position, id ->
+                val item = parent?.getItemAtPosition(position).toString()
+                Toast.makeText(context,item,Toast.LENGTH_SHORT).show()
+            }
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/stepcounterdemoapp/steps/StepsViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.stepcounterdemoapp.steps
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class StepsViewModel() : ViewModel() {
+
+    private val _isStarted = MutableLiveData<Boolean>()
+    val isStarted: LiveData<Boolean> = _isStarted
+
+    fun changeButtonState(){
+        _isStarted.postValue(_isStarted.value == false)
+    }
+}
