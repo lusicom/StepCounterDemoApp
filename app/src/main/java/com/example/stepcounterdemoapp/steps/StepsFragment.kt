0dcf7a1f5e4ff941ac40d381721a4728afb1e60c
Lucy Holub <lusicomgolub@gmail.com>
2021-11-17 12:44:58 +0200

    added compass

--- app/src/main/java/com/example/stepcounterdemoapp/steps/StepsFragment.kt
+++ app/src/main/java/com/example/stepcounterdemoapp/steps/StepsFragment.kt
@@ -13,11 +13,19 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.stepcounterdemoapp.R
 import com.example.stepcounterdemoapp.databinding.FragmentStepsBinding
+import java.lang.Math.toDegrees
 
 
 class StepsFragment : Fragment(){
 
     private lateinit var viewModel: StepsViewModel
+
+    private var arrayGravity = FloatArray(3)
+    private var arrayMagnetic = FloatArray(3)
+    private var orientation = FloatArray(3)
+    private var rotationMatrix = FloatArray(9)
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,25 +44,40 @@ class StepsFragment : Fragment(){
 
         val listener: SensorEventListener = object : SensorEventListener {
             override fun onSensorChanged(event: SensorEvent) {
-                if (event?.sensor?.type == Sensor.TYPE_GYROSCOPE) {
+                if (event.sensor?.type == Sensor.TYPE_GYROSCOPE) {
                     val gyroString = context?.resources?.getString(R.string.tv_gyro_data)
                     binding.gyroTxtView.text="$gyroString \nx: ${event.values[0]}\n"
                     binding.gyroTxtView.append("y: ${event.values[1]}\n")
                     binding.gyroTxtView.append("z: ${event.values[2]}")
                 }
 
-                if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+                if (event.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
                     val accString = context?.resources?.getString(R.string.tv_accelerometer_data)
                     binding.accTxtView.text="$accString \nx: ${event.values[0]}\n"
                     binding.accTxtView.append("y: ${event.values[1]}\n")
                     binding.accTxtView.append("z: ${event.values[2]}")
+                    arrayGravity = event.values
                 }
 
-                if (event?.sensor?.type == Sensor.TYPE_MAGNETIC_FIELD) {
-                    val magnString = context?.resources?.getString(R.string.tv_magnetometer_data)
-                    binding.magnTxtView.text="$magnString \nx: ${event.values[0]}\n"
-                    binding.magnTxtView.append("y: ${event.values[1]}\n")
-                    binding.magnTxtView.append("z: ${event.values[2]}")
+                if (event.sensor?.type == Sensor.TYPE_MAGNETIC_FIELD) {
+                    val compsString = context?.resources?.getString(R.string.tv_compass_data)
+                    arrayMagnetic = event.values
+                    SensorManager.getRotationMatrix(rotationMatrix, null, arrayGravity, arrayMagnetic)
+                    SensorManager.getOrientation(rotationMatrix, orientation)
+                    val degrees = (toDegrees(orientation[0].toDouble()) + 360).toFloat() % 360
+                    val compassOrient = when(degrees){
+                        in 0.0..23.0 -> "North"
+                        in 23.0..67.0 -> "North East"
+                        in 67.0..112.00-> "East"
+                        in 112.0..157.0 -> "South East"
+                        in 157.0..202.0 -> "South"
+                        in 202.0..247.0 -> "South West"
+                        in 247.0..292.0 -> "West"
+                        in 292.0..337.0 -> "North West"
+                        in 337.0..360.0 -> "North"
+                        else -> "Compass not active"
+                    }
+                    binding.compassTxtView.text = "$compsString\n$compassOrient\n$degrees"
                 }
             }
 
